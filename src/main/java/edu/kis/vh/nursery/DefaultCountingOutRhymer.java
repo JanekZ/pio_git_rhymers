0ebf3589226547219ae90c68667eb96c53188f7f
FKalin15 <filipkal55@gmail.com>
2025-03-17 21:22:42 +0100

    6. Zastapienie literalow deklaracjami stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int ERR_INT = -1;
 
-    public int total = -1;
+    private static final int NUMBERS_ARRAY_SIZE = 12;
+
+    private int[] numbers = new int[NUMBERS_ARRAY_SIZE];
+
+    public int total = ERR_INT;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERR_INT;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == NUMBERS_ARRAY_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERR_INT;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERR_INT;
         return numbers[total--];
     }
 
