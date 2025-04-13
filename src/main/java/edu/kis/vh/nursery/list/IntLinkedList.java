77b68462c89a6c8d3221df7cdbedcd25c6ac4479
Jan Zimniak <zimniak.janek@outlook.com>
2025-04-13 20:02:24 +0200

    Dodanie dokumentacji do klasy Node.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,32 +38,64 @@ public class IntLinkedList {
     }
 }
 
+/**
+ * A double linked list (DLL).
+ * Used by class {@link IntLinkedList}
+ * @see #value
+ * @see #prev
+ * @see #next
+ */
 class Node {
-
+    /**
+     * Values that the node holds.
+     */
     private final int value;
+    /**
+     * Previous node in DLL.
+     */
     private Node prev;
+    /**
+     * Next node in DLL.
+     */
     private Node next;
 
+    /**
+     * Constructor of this class, takes a value as a parameter.
+     * @param i
+     */
     public Node(int i) {
         value = i;
     }
 
+    /**
+     * @return Value that the Node holds.
+     */
     public int getValue() {
         return value;
     }
-
+    /**
+     * @return Previous Node in the DLL.
+     */
     public Node getPrev() {
         return prev;
     }
-
+    /**
+     * Links another Node as a previous Node.
+     * @params prev
+     */
     public void setPrev(Node prev) {
         this.prev = prev;
     }
-
+    /**
+     * @return Next Node in the DLL.
+     */
     public Node getNext() {
         return next;
     }
-
+    /**
+     * Links another Node as a next Node.
+     * @params prev
+     */
     public void setNext(Node next) {
         this.next = next;
     }
